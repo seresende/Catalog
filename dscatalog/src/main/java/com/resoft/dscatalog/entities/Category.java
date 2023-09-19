18730fabe9c28422015d617e446158bd7abf9e3c
Sérgio Luiz Resende <sergio.resende@compasso.com.br>
2023-09-19 16:50:11 -0300

    Category

---
+++ dscatalog/src/main/java/com/resoft/dscatalog/entities/Category.java
@@ -0,0 +1,15 @@
+package com.resoft.dscatalog.entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Category {
+
+    private Long id;
+    private String name;
+
+}
